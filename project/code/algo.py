324c0d0c158451e9b95600b38833170537c3c220
Loris Cousin <loris.cousin@student.isae-supaero.fr>
2025-04-23 08:42:24 +0200

    Copié collé dans agens, algo et evolution du code de evogym

---
+++ project/code/algo.py
@@ -0,0 +1,41 @@
+from imports import *
+def one_plus_lambda(config):
+    cfg = get_cfg(config["env_name"], robot=config["robot"]) # Get network dims
+    cfg = {**config, **cfg} # Merge configs
+    
+    env = make_env(cfg["env_name"], robot=cfg["robot"])
+
+    # Elite
+    elite = Agent(Network, cfg)
+
+    elite.fitness = evaluate(elite, env, max_steps=cfg["max_steps"])
+
+    fits = []
+    total_evals = []
+
+    bar = tqdm(range(cfg["generations"]))
+    for gen in bar:
+        population = [Agent(Network, cfg, genes=elite.mutate_ga()) 
+            for _ in range(cfg["lambda"])]
+
+        # with Pool(processes=len(population)) as pool:
+            # pop_fitness = pool.starmap(mp_eval, [(a, cfg) for a in population])
+
+        pop_fitness = [evaluate(a, env, max_steps=cfg["max_steps"]) for a in population]
+
+        best = np.argmax(pop_fitness)
+        best_fit = pop_fitness[best]
+        if best_fit > elite.fitness:
+            elite.genes = population[best].genes
+            elite.fitness = best_fit
+        fits.append(elite.fitness)
+        total_evals.append(len(population) * (gen+1))
+        bar.set_description(f"Best: {elite.fitness}")
+        
+    env.close()
+    plt.plot(total_evals, fits)
+    plt.xlabel("Evaluations")
+    plt.ylabel("Fitness")
+    plt.show()
+    return elite
+
