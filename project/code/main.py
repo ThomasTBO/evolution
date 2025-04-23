172cbaddba7f66df552867109db4e1da91609be8
Loris Cousin <loris.cousin@student.isae-supaero.fr>
2025-04-23 08:52:07 +0200

    complétion de main avec evogym

--- project/code/main.py
+++ project/code/main.py
@@ -1,1 +1,21 @@
-from imports import *
+from evolution import *
+
+
+if __name__ == "__main__":
+    
+    config = {
+    "env_name": "Walker-v0",
+    "robot": walker,
+    "generations": 10, # To change: increase!
+    "lambda": 10,
+    "max_steps": 100, # to change to 500
+    }
+
+    a = one_plus_lambda(config)
+    # a.fitness
+    env = make_env(config["env_name"], robot=config["robot"])
+    reward = evaluate(a, env, render=False)
+    print(f"Reward: {reward}")
+    env.close()
+   
+    np.save("Walker0.npy", a.genes)
